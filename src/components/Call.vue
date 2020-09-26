2743c18271f269cba95b74b788ae109d46d8f3bf
Natalya Ivanova <natalyas.ivanova@gmail.com>
2020-09-26 21:17:57 +0300

    Video play/pause/resume

--- src/components/Call.vue
+++ src/components/Call.vue
@@ -15,13 +15,21 @@
       </router-link>
     </nav>
 
-    <canvas id="canvas" />
-    <button
-      :class="['button', canStream && 'call-page__button']"
-      @click="toggleStream"
-    >
-      {{ buttonLabel }}
-    </button>
+    <div v-show="canStream" class="call-page__video">
+
+      <canvas ref="canvas" id="canvas" />
+
+      <video ref="video" width="300" height="200" autoplay />
+
+      <button
+        :class="['button', canStream && 'call-page__button']"
+        @click="toggleStream"
+      >
+        {{ buttonLabel }}
+      </button>
+    </div>
+
+    <p v-if="!canStream" v-html="errorMessage" />
   </section>
 </template>
 
@@ -35,26 +43,69 @@ export default {
       resumeLabel: 'Resume video',
       pauseLabel: 'Pause video',
       canStream: false,
-      isStreaming: true
+      isStreaming: true,
+      canvas: null,
+      constraints: { video: true },
+      errorMessage: 'Sorry, no access to web camera.'
     }
   },
+
   computed: {
     buttonLabel () {
       return this.isStreaming ? this.pauseLabel : this.resumeLabel
     }
   },
-  mounted () {
+
+  async mounted () {
     this.canStream = checkNavigator()
+    if (this.canStream) {
+      await this.$nextTick()
+      this.canvas = this.$refs.canvas
+      this.getStream()
+    }
   },
+
   methods: {
     toggleStream () {
+      this.isStreaming = !this.isStreaming
+      if (this.isStreaming) this.getStream()
+    },
 
+    async getStream () {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(this.constraints)
+        this.paint(stream)
+      } catch (e) {
+        this.canStream = false
+        console.error(e)
+      }
+    },
+
+    paint (stream) {
+      const { video } = this.$refs
+      if (video) video.srcObject = stream
+      const _this = this
+      const ctx = this.canvas.getContext('2d')
+      const af = requestAnimationFrame(draw)
+
+      function draw () {
+        if (_this.isStreaming) {
+          _this.canvas.width = video.videoWidth
+          _this.canvas.height = video.videoHeight
+          ctx.drawImage(video, 0, 0, _this.canvas.width, _this.canvas.height)
+          requestAnimationFrame(draw)
+        } else cancelAnimationFrame(af)
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+  video {
+    display: none;
+  }
+
   #canvas {
     width: 500px;
     height: 400px;
